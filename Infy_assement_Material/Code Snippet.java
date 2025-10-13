6c2c5d93d37636e2854532f8a820d1dbc42d468f
Rohankumar Dyavanpally <dyavanpallyrohan@gmail.com>
2025-10-13 15:50:30 +0530

    test from infosys

--- Infy_assement_Material/Code Snippet.java
+++ Infy_assement_Material/Code Snippet.java
@@ -7,7 +7,7 @@ class Q1 {
     public static void main(String[] args) {
         int n = 7;
         if (n % 2 == 0)
-            System.out.println("Even");
+            System  .out.println("Even");
         else
             System.out.println("Odd");
     }
